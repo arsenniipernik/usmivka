9ef741bba81f3da35e1c8fa77b1a63057219795f
ArtemZankovskiy <artem.zankovskiy@infinno.eu>
2023-01-07 20:45:27 +0200

    Changes in contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -9,7 +9,7 @@ export default function ContactPage() {
             <br />
             Имейл адрес: detskashkolausmivka@gmail.com
             <br />
-            <a style={{color: "var(--very-light-cyan)"}} href="https://www.facebook.com/profile.php?id=100089185053577&mibextid=ZbWKwL" rel="external noopener noreferrer" target="_blank" >Фейсбук</a>
+            <a style={{ color: "var(--very-light-cyan)" }} href="https://www.facebook.com/profile.php?id=100089185053577&mibextid=ZbWKwL" rel="external noopener noreferrer" target="_blank" >Фейсбук</a>
 
             <Subtitle><strong style={{ color: "white" }}>
                 Уроците се провеждат в зала 1 ул Теменуга 1б ж.к Слатина (до пазарчето)
